471c05b71dab8d3c6bb4b4a6e35763f2be47ca82
Tinker26 <umarnurxodjayev17@gmail.com>
2021-12-21 11:30:14 +0500

    shablon add

--- grid/views.py
+++ grid/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.shortcuts import get_object_or_404, render
 from category.models import Category
 from .models import *
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.db.models import Q
 # Create your views here.
 def grid(request, category_slug=None):
     if category_slug == None:
@@ -10,8 +12,11 @@ def grid(request, category_slug=None):
         categories = get_object_or_404(Category, slug=category_slug)
         products = Product_grid.objects.filter(is_available=True, category=categories)
 
+    paginator = Paginator(products, 3)
+    page = request.GET.get('page')
+    paged_products = paginator.get_page(page)
     context = {
-        'products': products,
+        'products': paged_products,
         'product_count': products.count()
     }
     return render(request, 'blog_grid.html', context)
--- habibi/views.py
+++ habibi/views.py
@@ -5,6 +5,9 @@ from store.models import Product
 from django.shortcuts import get_object_or_404, render
 from category.models import Category
 from grid.models import *
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.db.models import Q
+
 def Home(request, category_slug=None):
     if category_slug == None:
         products = Product.objects.filter(is_available=True)
@@ -12,8 +15,11 @@ def Home(request, category_slug=None):
         categories = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.filter(is_available=True, category=categories)
 
+    paginator = Paginator(products, 5)
+    page = request.GET.get('page')
+    paged_products = paginator.get_page(page)
     context = {
-        'products': products,
+        'products': paged_products,
         'product_count': products.count()
     }
     return render(request, 'home.html', context)
